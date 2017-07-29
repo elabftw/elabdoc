7d42ee94bf76f9f030d3ae067b7782a61f120abd
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-07-29 21:55:19 +0200

    remove the api grunt cmd

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,15 +10,11 @@ module.exports = function(grunt) {
     shell: {
       builddoc: {
         command: 'cd doc; make html'
-      },
-      buildapi: {
-        command: 'phpdoc run -d ../elabftw/app/classes -d ../elabftw/app/models -d ../elabftw/app/controllers -d ../elabftw/app/views -t api'
-      },
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('doc', 'shell:builddoc');
-  grunt.registerTask('api', 'shell:buildapi');
 };
