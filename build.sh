bda5924703d83d929fd48e3974b1b2f710972e62
Nicolas CARPi <nico-git@deltablot.email>
2020-06-04 22:08:00 +0200

    docker code is now at deltablot/doc.elabftw.net

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker build --no-cache -t elabftw/elabdoc-builder .
-cid=$(docker create elabftw/elabdoc-builder)
-docker cp "$cid:/home/node/build/doc" build
-docker rm "$cid"
