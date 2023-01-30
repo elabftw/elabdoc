cfb3ed14d46a8b986ccb72d2e4246476e453a0a2
Nicolas CARPi <nico-git@deltablot.email>
2023-01-31 00:59:48 +0100

    add jquery extension

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,9 @@ StandaloneHTMLBuilder.supported_image_types = ['image/gif', 'image/svg+xml', 'im
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinxcontrib.jquery",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
